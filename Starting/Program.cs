d0deee7c3d2a16754d6bcfccb75109c4facd5276
Shafighipoor <shafighipoor@gmail.com>
2025-04-23 14:05:22 +0330

    Init

---
+++ Starting/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Starting {
+    internal class Program {
+        static void Main(string[] args) {
+            
+            MakeFibonacciSeries();
+
+        }
+
+        private static void MakeFibonacciSeries() {
+            int n = 10;
+            int a = 0, b = 1;
+            for (int i = 0; i < n; i++) {
+                Console.WriteLine(a);
+                int temp = a + b;
+                a = b;
+                b = temp;
+            }
+
+            Console.WriteLine("");
+            Console.WriteLine("Mission Completed !");
+            Console.WriteLine("");
+            Console.WriteLine("Press any key to Exit");
+
+            Console.ReadKey();
+
+            Environment.Exit(0);
+        }
+    }
+}
